ec6494d1841123870ac29fddaf01d35c41489383
VinhNguyenD23 <nguyenthanhvinh31305@gmail.com>
2024-10-16 01:10:13 +0700

    Update Console

--- src/console.cpp
+++ src/console.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void menu(){
+void menu()
+{
     cout << "\n\t\t==========MENU==========";
     cout << "\n\t\t1. QUAN LY VAT TU";
     cout << "\n\t\t2. QUAN LY NHAN VIEN";
@@ -10,12 +11,14 @@ void menu(){
     cout << "\n\t\t=======================\n";
 
     int choice;
-    do{
+    do
+    {
         cout << "NHAP LUA CHON: ";
         cin >> choice;
-        switch (choice){
+        switch (choice)
+        {
 
-        case 1: 
+        case 1:
             cout << "\n\t\t==========QUAN LY VAT TU==========";
             cout << "\n\t\t1. THEM VAT TU";
             cout << "\n\t\t2. CHINH SUA VAT TU";
@@ -26,32 +29,33 @@ void menu(){
             cout << "\n\t\t=======================\n";
 
             int choiceVT;
-            do{
+            do
+            {
                 cout << "NHAP LUA CHON: ";
                 cin >> choiceVT;
                 switch (choiceVT)
                 {
                 case 1:
-                    
+
                     break;
                 case 2:
-                    
+
                     break;
                 case 3:
-                    
+
                     break;
                 case 4:
-                    
+
                     break;
                 case 5:
-                    
+
                     break;
-                
+
                 default:
                     cout << "LUA CHON KHONG HOP LE!!! VUI LONG CHON LAI";
                     break;
                 }
-            }while(choiceVT !=0);
+            } while (choiceVT != 0);
             break;
 
         case 2:
@@ -65,32 +69,33 @@ void menu(){
             cout << "\n\t\t=======================\n";
 
             int choiceNV;
-            do{
+            do
+            {
                 cout << "NHAP LUA CHON: ";
                 cin >> choiceNV;
                 switch (choiceNV)
                 {
                 case 1:
-                    
+
                     break;
                 case 2:
-                    
+
                     break;
                 case 3:
-                    
+
                     break;
                 case 4:
-                    
+
                     break;
                 case 5:
-                    
+
                     break;
-                
+
                 default:
-                    cout << "LUA CHON KHONG HOP LE!!! VUI LONG CHON LAI"; 
+                    cout << "LUA CHON KHONG HOP LE!!! VUI LONG CHON LAI";
                     break;
                 }
-            }while(choiceNV !=0);
+            } while (choiceNV != 0);
             break;
 
         case 3:
@@ -100,42 +105,43 @@ void menu(){
             cout << "\n\t\t3. XOA HOA DON";
             cout << "\n\t\t4. TIM KIEM HOA DON";
             cout << "\n\t\t5. IN DANH SACH HOA DON";
+            cout << "\n\t\t6. XEM CHI TIET HOA DON";
             cout << "\n\t\t0. EXIT";
             cout << "\n\t\t=======================\n";
 
             int choiceHD;
-            do{
+            do
+            {
                 cout << "NHAP LUA CHON: ";
                 cin >> choiceHD;
                 switch (choiceHD)
                 {
                 case 1:
-                    
+
                     break;
                 case 2:
-                    
+
                     break;
                 case 3:
-                    
+
                     break;
                 case 4:
-                    
+
                     break;
                 case 5:
-                    
+
                     break;
-                
+
                 default:
-                    cout << "LUA CHON KHONG HOP LE!!! VUI LONG CHON LAI"; 
+                    cout << "LUA CHON KHONG HOP LE!!! VUI LONG CHON LAI";
                     break;
                 }
-            }while(choiceHD !=0);
+            } while (choiceHD != 0);
             break;
 
         default:
             cout << "LUA CHON KHONG HOP LE!!! VUI LONG CHON LAI";
             break;
         }
-    }while(choice != 0);
-
+    } while (choice != 0);
 }

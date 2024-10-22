13590fc8f28768fee00415dc65877140a6b382fc
trinhlong1201 <songlong12012005@gmail.com>
2024-10-23 03:19:37 +0700

    hihi

--- src/model/Facilities.cpp
+++ src/model/Facilities.cpp
@@ -1,9 +1,50 @@
 #include "Facilities.h"
+#include<vector>
+#include<fstream>
+#include<sstream>
+
 
 FacilitiesModel::FacilitiesModel()
 {
     // TODO: Read data from databases [*.csv] in ../databases [ priority: high ]
-    this->data = nullptr;
+    // this->data = nullptr;
+    this -> data = AVLTree<Facility, std::string>;
+    std::fstream f("../databases/Facilities.csv", std::ios::in | std::ios::out);
+    std::string line;
+    if(f.is_open())
+    {
+        while (getline(f, line))
+        {
+            std::istringstream fileData(line);
+            std::string tempData;
+            Facility getUser;
+            AVLTree<std::string> facilities;
+            Node* root = new node();
+            std::string quantity_str;
+            // while (getline(fileData, tempData, ','))
+            // {
+            //     temp.insert(tempData);
+            // }
+            // std::cout << std::endl;
+            std::getline(fileData, getUser.id, ',');
+            std::getline(fileData, getUser.name, ',');
+            std::getline(fileData, getUser.unit, ',');
+            std::getline(fileData, quantity_str, ',');
+
+            // Convert quantity from string to long
+            getUser.quantity = std::stol(quantity_str);
+
+            // Insert the facility into the AVL tree
+            root = facilities.insert(root, getUser);
+        }
+
+    f.close();
+    return root;
+    }
+    else
+    {
+        throw new std::runtime_error("[ERROR] This database not found or broken, please try again !");
+    }
 }
 
 FacilitiesModel::~FacilitiesModel()
--- src/model/Staffs.cpp
+++ src/model/Staffs.cpp
@@ -4,6 +4,37 @@ StaffsModel::StaffsModel()
 {
     this->data = new DArray<Staff>[MAX_STAFF];
     // TODO: Read data from databases [*.csv] in ../databases [ priority: high ]
+    std::fstream f("../databases/staff.csv", std::ios::in | std::ios::out);
+    std::string line;
+    if(f.is_open())
+    {
+        while (getline(f, line))
+        {
+            std::istringstream fileData(line);
+            std::string tempData;
+            Staff getUser;
+            DArray<std::string> staff;
+            // while (getline(fileData, tempData, ','))
+            // {
+            //     temp.push(tempData);
+            // }
+            // std::cout << std::endl;
+            std::string genderStr;
+            std::getline(fileData, getUser.id, ',');
+            std::getline(fileData, getUser.firstName, ',');
+            std::getline(fileData, getUser.lastName, ',');
+            std::getline(fileData, genderStr, ',');
+
+            getUser.gender = (genderStr == "1"); // Chuyển chuỗi giới tính thành kiểu bool
+
+            staff.push(getUser);
+    }
+    f.clone();
+        }
+    else
+    {
+        throw new std::runtime_error("[ERROR] This database not found or broken, please try again !");
+    }
 }
 
 StaffsModel::~StaffsModel()

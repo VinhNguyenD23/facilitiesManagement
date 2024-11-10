7d2f7a9650e8ce374d49fffd51c1840ad39da80d
trinhlong1201 <155670722+trinhlong1201@users.noreply.github.com>
2024-11-10 16:32:27 +0700

    staff controller

--- src/controller/InvoiceController.cpp
+++ src/controller/InvoiceController.cpp
@@ -1,6 +1,35 @@
 #include "InvoiceController.h"
 
 InvoiceController::InvoiceController()
+{this->invoiceService = new InvoiceService();
+}
+
+void InvoiceController::createNewInvoice(Invoice data)
+{
+    this->invoiceService->create(data);
+}
+
+void InvoiceController::updateExistInvoice(Invoice data)
+{
+    this->invoiceService->update(data);
+}
+
+void InvoiceController::removeInvoice(Invoice data)
+{
+    this->invoiceService->remove(data);
+}
+
+LinkedList<Invoice> *InvoiceController::getListInvoices()
+{
+    return nullptr;
+}
+
+Invoice *InvoiceController::getInvoiceById(QString id)
+{
+    return nullptr;
+}
+
+InvoiceController::~InvoiceController()
 {
 }
 
--- src/controller/StaffController.cpp
+++ src/controller/StaffController.cpp
@@ -2,6 +2,33 @@
 
 StaffController::StaffController()
 {
+    this->staffService = new StaffService();
+}
+
+void StaffController::create(Staff data)
+{
+    this->staffService->create(data);
+}
+
+void StaffController::updateExistStaff(Staff data)
+{
+    this->StaffService->update(data);
+}
+
+void StaffController::removeStaff(Staff data)
+{
+    this->StaffService->remove(data);
+}
+
+DArray<Staff> *StaffController::getListStaff()
+{
+    return nullptr;
+}
+
+Staff *StaffController::getStaffById(QString id)
+{
+    return nullptr;
+
 }
 
 StaffController::~StaffController()
